3d5ad925a418806de041fc150098d6d1ebb554a5
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-07 23:41:06 -0300

    Implemento la configuracion del modo de la uart

--- uart/src/uart.c
+++ uart/src/uart.c
@@ -1,8 +1,18 @@
 #include "uart.h"
 
+#define MODE_8_BITS 	1
+#define MODE_PAR_NONE	1
+#define MODE_STOP_1_BIT	1
+
 static uint32_t TxReg;
- 
+static uint32_t ModeReg;
+
 uint8_t Uart_ConfigureIO(void) {
 	TxReg = 0;
 	return 1;
+}
+
+uint8_t Uart_ConfigureMode(void) {
+	ModeReg = MODE_8_BITS | MODE_PAR_NONE | MODE_STOP_1_BIT;
+	return 1;	
 }
