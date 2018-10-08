5dab0547cb5f2486d0112159c0e49eac18e2a9ba
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-07 23:47:53 -0300

    Agrego un nuevo test para probar el envio de un string a través de la uart.

--- uart/test/test_uart.c
+++ uart/test/test_uart.c
@@ -20,3 +20,9 @@ void test_ConfigureMode(void) {
 void test_ConfigureBaudRate(void) {
 	TEST_ASSERT_EQUAL(1, Uart_ConfigureBaudrate(115200));
 }
+
+void test_TrasnmitString(void) {
+	uint8_t string[] = {'H', 'O', 'L', 'A', '\0'};
+
+	TEST_ASSERT_EQUAL(4, Uart_TransmitString(string));
+}
