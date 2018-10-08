cddf750b2545a9f3e65547f9e65b6f16f5dd8da2
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-07 23:46:08 -0300

    Implemento la configuracion del baudrate de la uart.

--- uart/src/uart.c
+++ uart/src/uart.c
@@ -6,6 +6,7 @@
 
 static uint32_t TxReg;
 static uint32_t ModeReg;
+static uint32_t BaudReg;
 
 uint8_t Uart_ConfigureIO(void) {
 	TxReg = 0;
@@ -15,4 +16,9 @@ uint8_t Uart_ConfigureIO(void) {
 uint8_t Uart_ConfigureMode(void) {
 	ModeReg = MODE_8_BITS | MODE_PAR_NONE | MODE_STOP_1_BIT;
 	return 1;	
+}
+
+uint8_t Uart_ConfigureBaudrate(uint32_t baudrate) {
+	BaudReg = baudrate;
+	return 1;
 }
