2bc3629480cd4ea01847bc3198cabd60c5518b97
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 23:01:40 -0300

    Corrijo el error de compilacion declarando la funcion que permite enviar un string a través de la uart en el archivo de cabecera del modulo.

--- uart/src/uart.h
+++ uart/src/uart.h
@@ -3,3 +3,4 @@
 uint8_t Uart_ConfigureIO(uint32_t *uartReg);
 uint8_t Uart_ConfigureMode(uint8_t dataBitsMode, uint8_t parityBitsMode, uint8_t stopBitsMode);
 uint8_t Uart_ConfigureBaudRate(uint8_t baudrateBits);
+uint32_t Uart_TransmitString(uint8_t *string);
