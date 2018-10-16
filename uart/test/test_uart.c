04ce12d85c858d8961101c1fc090fbf966bb1a67
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:50:57 -0300

    Agrego un nuevo test para probar que el baudrate debe poder ser configurado por el usuario.

--- uart/test/test_uart.c
+++ uart/test/test_uart.c
@@ -42,3 +42,42 @@ void test_ConfigureUARTMode(void) {
 	TEST_ASSERT_EQUAL_HEX32(0xFFFFFC3E, UartReg);
 }
 
+void test_ConfigureUARTBaudrate(void) {
+	uint32_t UartReg = 0xFFFFFFFF;
+	enum data_mode {
+		FIVE_BITS = 0x00,
+		SIX_BITS,
+		SEVEN_BITS,
+		EIGHT_BITS
+	};
+
+	enum parity_mode {
+		NONE = 0x00,
+		ODD,
+		EVEN
+	};
+
+	enum stop_mode {
+		ONE_BIT = 0x00,
+		ONE_AND_HALF_BITS,
+		TWO_BITS
+	};
+
+	enum baud {
+		BAUD_2400 = 0x00,
+		BAUD_4800,
+		BAUD_9600, 
+		BAUD_38400,
+		BAUD_57600,
+		BAUD_115200,
+		BAUD_128000,
+		BAUD_256000
+	};
+
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureIO(&UartReg));
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureMode(EIGHT_BITS, NONE, ONE_BIT));
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureBaudRate(BAUD_115200));
+	TEST_ASSERT_EQUAL_HEX32(0xFFFFDC3E, UartReg);
+}
+
+
