61f4aee74330d41790c2d2b68180888e48db0b84
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:51:45 -0300

    Corrijo el error de compilacion declarando la funcion que permite configurar el baudrate en el archivo de cabecera del modulo.

--- uart/src/uart.h
+++ uart/src/uart.h
@@ -2,3 +2,4 @@
 
 uint8_t Uart_ConfigureIO(uint32_t *uartReg);
 uint8_t Uart_ConfigureMode(uint8_t dataBitsMode, uint8_t parityBitsMode, uint8_t stopBitsMode);
+uint8_t Uart_ConfigureBaudRate(uint8_t baudrateBits);
