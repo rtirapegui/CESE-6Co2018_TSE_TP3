08c6b96a5b36954eb3cadb00fbb45dc42ba7a1db
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:49:51 -0300

    Corrijo el fallo del test implementando la funcionalidad requerida en la funcion.

--- uart/src/uart.c
+++ uart/src/uart.c
@@ -1,12 +1,26 @@
 #include "uart.h"
 
-#define IO_BIT_MASK	0x00000001
+static union UARTRegister {
+	struct {
+		uint8_t IO_BIT:				 1;
+		uint8_t RESERVED_1:		    3;
+		uint8_t DATA_BITS_MODE:		 2;
+		uint8_t PARITY_BITS_MODE:	 2;
+		uint8_t STOP_BITS_MODE:		 2;
+		uint32_t RESERVED:			22;
+	} bf;
+	uint32_t word;
+} *UARTReg;
 
 uint8_t Uart_ConfigureIO(uint32_t *uartReg) {
-	*uartReg &= ~IO_BIT_MASK;
+	UARTReg = (union UARTRegister *) uartReg;
+	UARTReg->bf.IO_BIT = 0;
 	return 1;
 }
 
 uint8_t Uart_ConfigureMode(uint8_t dataBitsMode, uint8_t parityBitsMode, uint8_t stopBitsMode) {
-
+	UARTReg->bf.DATA_BITS_MODE = dataBitsMode;
+	UARTReg->bf.PARITY_BITS_MODE = parityBitsMode;
+	UARTReg->bf.STOP_BITS_MODE = stopBitsMode;
+	return 1;
 }
