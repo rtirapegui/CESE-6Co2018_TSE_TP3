1a2f227846271e56ca762d93d88c7d5883a2b02c
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:49:08 -0300

    Corrijo el error de enlazador implementando una funcion vacia.

--- uart/src/uart.c
+++ uart/src/uart.c
@@ -6,3 +6,7 @@ uint8_t Uart_ConfigureIO(uint32_t *uartReg) {
 	*uartReg &= ~IO_BIT_MASK;
 	return 1;
 }
+
+uint8_t Uart_ConfigureMode(uint8_t dataBitsMode, uint8_t parityBitsMode, uint8_t stopBitsMode) {
+
+}
