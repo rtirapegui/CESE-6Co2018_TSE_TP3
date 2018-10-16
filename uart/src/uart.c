9d5a793fe7e8251758e4b398a94afe9bcf304c02
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:44:40 -0300

    Corrijo el fallo del test implementando la funcionalidad requerida en la funcion.

--- uart/src/uart.c
+++ uart/src/uart.c
@@ -1,5 +1,8 @@
 #include "uart.h"
 
-uint8_t Uart_ConfigureIO(uint32_t *uartReg) {
+#define IO_BIT_MASK	0x00000001
 
+uint8_t Uart_ConfigureIO(uint32_t *uartReg) {
+	*uartReg &= ~IO_BIT_MASK;
+	return 1;
 }
