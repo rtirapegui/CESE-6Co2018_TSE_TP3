82381101112a3dfff13dcdc2c9018de12b4f01d6
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:40:53 -0300

    Cambio de test para probar la primera funcionalidad: al iniciar el procesador no hay un estado conocido del hardware de la UART, por lo que debe ser iniciada con el pin Tx colocado en 0.

--- uart/test/test_uart.c
+++ uart/test/test_uart.c
@@ -9,5 +9,8 @@ void tearDown(void) {
 }
 
 void test_ConfigureUARTIO(void) {
-	TEST_FAIL_MESSAGE("Strated");
+	uint32_t UartReg = 0xFFFFFFFF;
+
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureIO(&UartReg));
+	TEST_ASSERT_EQUAL_HEX32(0xFFFFFFFE, UartReg);
 }
