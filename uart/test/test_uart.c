5270d1db2583d3b6bb38edbc9785a76c0efb007a
Rodrigo <rtirapeg@itba.edu.ar>
2018-10-15 22:47:44 -0300

    Agrego un nuevo test para probar la configuracion 8N1 (ocho (8) bits de datos, ningun (N) bit de paridad y un (1) bit de parada).

--- uart/test/test_uart.c
+++ uart/test/test_uart.c
@@ -15,3 +15,30 @@ void test_ConfigureUARTIO(void) {
 	TEST_ASSERT_EQUAL(1, Uart_ConfigureIO(&UartReg));
 	TEST_ASSERT_EQUAL_HEX32(0xFFFFFFFE, UartReg);
 }
+
+void test_ConfigureUARTMode(void) {
+	uint32_t UartReg = 0xFFFFFFFF;
+	enum data_mode {
+		FIVE_BITS = 0x00,
+		SIX_BITS,
+		SEVEN_BITS,
+		EIGHT_BITS
+	};
+
+	enum parity_mode {
+		NONE = 0x00,
+		ODD,
+		EVEN
+	};
+
+	enum stop_mode {
+		ONE_BIT = 0x00,
+		ONE_AND_HALF_BITS,
+		TWO_BITS
+	};
+
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureIO(&UartReg));
+	TEST_ASSERT_EQUAL(1, Uart_ConfigureMode(EIGHT_BITS, NONE, ONE_BIT));
+	TEST_ASSERT_EQUAL_HEX32(0xFFFFFC3E, UartReg);
+}
+
